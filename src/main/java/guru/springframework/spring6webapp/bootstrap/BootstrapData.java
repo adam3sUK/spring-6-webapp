e130e6e301d0ff3c98f58f8d6823a320db32e1a2
Adam Harbisher <aharbisher@deloitte.co.uk>
2023-05-11 06:07:52 +0100

    Added Publisher entity

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -5,18 +5,26 @@ import org.springframework.stereotype.Component;
 
 import guru.springframework.spring6webapp.domain.Author;
 import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
 import guru.springframework.spring6webapp.repositories.AuthorRepository;
 import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
 
 @Component
 public class BootstrapData implements CommandLineRunner {
 
   private final AuthorRepository authorRepository;
   private final BookRepository bookRepository;
+  private final PublisherRepository publisherRepository;
 
-  public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+  public BootstrapData(
+    AuthorRepository authorRepository, 
+    BookRepository bookRepository,
+    PublisherRepository publisherRepository
+    ) {
     this.authorRepository = authorRepository;
     this.bookRepository = bookRepository;
+    this.publisherRepository = publisherRepository;
   }
 
   @Override
@@ -29,8 +37,16 @@ public class BootstrapData implements CommandLineRunner {
       ddd.setTitle("Learning Java for Dummies");
       ddd.setIsbn("123456");
 
+      Publisher penguin = new Publisher();
+      penguin.setPublisherName("Penguin");
+      penguin.setAddress("42 Wallaby Way");
+      penguin.setCity("Syndey");
+      penguin.setState("Florida");
+      penguin.setZip("1234");
+
       Author adamSaved = authorRepository.save(adam);
       Book dddSaved = bookRepository.save(ddd);
+      publisherRepository.save(penguin);
 
       Author george = new Author();
       george.setFirstName("George");
@@ -46,9 +62,14 @@ public class BootstrapData implements CommandLineRunner {
       georgeSaved.getBooks().add(animalFarmSaved);
       adamSaved.getBooks().add(dddSaved);
 
+      authorRepository.save(georgeSaved);
+      authorRepository.save(adamSaved);
+
       System.out.println("In Bootstrap");
 
-      System.out.println("author count:" + authorRepository.count());
+      System.out.println("author count: " + authorRepository.count());
+
+      System.out.println("publisher count: " + publisherRepository.count());
   }
 }
 
